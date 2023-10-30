1b8305beced613483a1863f03f1cef1ab7f8fd0e
kaicikoglu <kaicikoglu@outlook.de>
2023-10-30 17:16:06 +0100

    03-HTML

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,7 +3,9 @@ package controllers
 import com.google.inject.Guice
 import de.htwg.se.minesweeper.MinesweeperModuleEasy
 import de.htwg.se.minesweeper.controller.controllerComponent.ControllerInterface
+import de.htwg.se.minesweeper.controller.controllerComponent.controllerBaseImpl.Controller
 import de.htwg.se.minesweeper.model.FieldComponent.FieldBaseImpl.Coordinates
+import de.htwg.se.minesweeper.util.DifficultyFactory
 import play.api.mvc._
 
 import javax.inject._
@@ -15,31 +17,39 @@ import javax.inject._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
+
   private val injector = Guice.createInjector(new MinesweeperModuleEasy)
-  val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
+  var controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
 
   def index(): Action[AnyContent] = Action {
     Ok(views.html.index())
   }
 
-  def createGame(difficulty: String): Action[AnyContent] = Action {
-    Ok(views.html.displayGame(controller.createNewField(difficulty)))
+  def easy(): Action[AnyContent] = Action {
+    controller = Controller(DifficultyFactory("1").run)
+    controller.setBombs(controller.calculateBombAmount())
+    Ok(views.html.displayGame(controller))
+  }
+
+  def hard(): Action[AnyContent] = Action {
+    controller = Controller(DifficultyFactory("2").run)
+    controller.setBombs(controller.calculateBombAmount())
+    Ok(views.html.displayGame(controller))
+  }
+
+  def extreme(): Action[AnyContent] = Action {
+    controller = Controller(DifficultyFactory("3").run)
+    controller.setBombs(controller.calculateBombAmount())
+    Ok(views.html.displayGame(controller))
   }
 
   def revealValue(x: Integer, y: Integer): Action[AnyContent] = Action {
-
-    Ok(views.html.displayGame(controller.revealValue(new Coordinates(x, y))))
+    controller.doAndPublish(controller.revealValue(new Coordinates(x, y)))
+    Ok(views.html.displayGame(controller))
   }
 
   def setFlag(x: Integer, y: Integer): Action[AnyContent] = Action {
-    Ok(views.html.displayGame(controller.setFlag(new Coordinates(x, y))))
-  }
-
-  def undo(): Action[AnyContent] = Action {
-    Ok(views.html.displayGame(controller.undo))
-  }
-
-  def redo(): Action[AnyContent] = Action {
-    Ok(views.html.displayGame(controller.redo))
+    controller.doAndPublish(controller.setFlag(new Coordinates(x, y)))
+    Ok(views.html.displayGame(controller))
   }
 }
